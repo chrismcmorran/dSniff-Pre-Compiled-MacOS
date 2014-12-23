bf70e4dc9a624cd2a63053b8ba321255e38dbb68
Chris McMorran <Chris@Chriss-MacBook-Pro.local>
2014-12-22 23:16:32 -0500

    Added remaining dsniff suite binaries.

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+mv arpspoof /usr/local/bin/
+mv dsniff /usr/local/bin/
+mv filesnarf /usr/local/bin/
+mv macof /usr/local/bin/
+mv mailsnarf /usr/local/bin/
+mv msgsnarf /usr/local/bin/
+
+alias arpspoof="/usr/local/bin/arpspoof"
+alias dsniff="/usr/local/bin/dsniff"
+alias filesnarf="/usr/local/bin/filesnarf"
+alias macof="/usr/local/bin/macof"
+alias mailsnarf="/usr/local/bin/mailsnarf"
+alias msgsnarf="/usr/local/bin/msgsnarf"
+echo "Install Complete"
